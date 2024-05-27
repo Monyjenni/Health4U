4d1582debc271d52438bffa92a4dd01c767fd4ce
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-05-27 17:43:36 +0700

    Adding func to open Youtube

--- app/src/main/java/com/example/health4u_v1/bmi_result.kt
+++ app/src/main/java/com/example/health4u_v1/bmi_result.kt
@@ -1,11 +1,19 @@
 package com.example.health4u_v1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class bmi_result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_result)
     }
+
+    fun navigateBackToDashboard(view: View) {
+        val intent = Intent(this, dashboard_screen::class.java)
+        startActivity(intent)
+    }
+
 }
