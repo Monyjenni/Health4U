3071e4fffb8f9a633c99746d480fd100b2f25903
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-05-27 14:16:22 +0700

    workout_screen

--- app/src/main/java/com/example/health4u_v1/dashboard_screen.kt
+++ app/src/main/java/com/example/health4u_v1/dashboard_screen.kt
@@ -1,11 +1,25 @@
 package com.example.health4u_v1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class dashboard_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_screen)
+    }
+
+    fun onWorkoutPlansClicked(view: View) {
+        val intent = Intent(this, workout_page1::class.java)
+        startActivity(intent)
+    }
+
+    fun onDietPlansClicked(view: View) {
+        val intent = Intent(this, DishPreview::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/health4u_v1/workout_preview.kt
+++ app/src/main/java/com/example/health4u_v1/workout_preview.kt
@@ -1,11 +1,23 @@
 package com.example.health4u_v1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class workout_preview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout_preview)
+    }
+
+    fun navigateToWorkoutDetail(view: View) {
+        val intent = Intent(this, workout_detail::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateBackToDashboard(view: View) {
+        val intent = Intent(this, dashboard_screen::class.java)
+        startActivity(intent)
     }
 }
